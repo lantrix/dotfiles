13922c8a7d1e075f2fe56aac89311d7a8b023f44
Lantrix <lantrix@pobox.com>
2015-10-09 12:55:27 +1100

    Java set on mac only if /usr/libexec/java_home exists

--- home/.bash_profile.d/005-java.sh
+++ home/.bash_profile.d/005-java.sh
@@ -1,1 +1,4 @@
-[[ $(/usr/libexec/java_home -v 1.8) == *jdk* ]] && export JAVA_HOME=$(/usr/libexec/java_home -v 1.8)
+#Mac set Java Home
+if [[ -f /usr/libexec/java_home ]]; then
+	[[ $(/usr/libexec/java_home -v 1.8) == *jdk* ]] && export JAVA_HOME=$(/usr/libexec/java_home -v 1.8)
+fi
