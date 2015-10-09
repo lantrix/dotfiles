36e6fb82581aca449b7548aa2905e05e7007273e
Lantrix <lantrix@pobox.com>
2015-10-09 12:52:35 +1100

    Bash completion on mac and linux

--- home/.bash_profile.d/001-bash-completion.sh
+++ home/.bash_profile.d/001-bash-completion.sh
@@ -1,3 +1,11 @@
-if [[ -d $BREW_PREFIX/etc/bash_completion.d ]]; then
-  source $BREW_PREFIX/etc/bash_completion.d/*
+#Mac Completion
+if [[ ! ${#BREW_PREFIX} -eq 0 ]]; then
+	if [[ -d $BREW_PREFIX/etc/bash_completion.d ]]; then
+  		source $BREW_PREFIX/etc/bash_completion.d/*
+	fi
+fi
+
+#Linux Completion
+if [[ -f /etc/bash_completion ]]; then
+	bash /etc/bash_completion
 fi
