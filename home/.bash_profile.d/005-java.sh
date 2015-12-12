5bd1c44a910f11b7e6b70880609adf3708089e86
Lantrix <lantrix@pobox.com>
2015-12-12 15:27:33 +1100

    Java handling when no JDKs

--- home/.bash_profile.d/005-java.sh
+++ home/.bash_profile.d/005-java.sh
@@ -1,4 +1,9 @@
+java_version=1.8
+
 #Mac set Java Home
 if [[ -f /usr/libexec/java_home ]]; then
-	[[ $(/usr/libexec/java_home -v 1.8) == *jdk* ]] && export JAVA_HOME=$(/usr/libexec/java_home -v 1.8)
+	output=`/usr/libexec/java_home -v $java_version 2>&1`
+	if [ $? -eq 0 ]; then
+		[[ $(/usr/libexec/java_home -v $java_version) == *jdk* ]] && export JAVA_HOME=$(/usr/libexec/java_home -v $java_version)
+	fi
 fi
