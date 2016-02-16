8e6a262e2923a44d411b7b1b5ca2c32246752ebb
Lantrix <lantrix@pobox.com>
2016-02-16 13:00:12 +1100

    New AWS Function for watching stacks

--- home/.bash_profile.d/003-aws.sh
+++ home/.bash_profile.d/003-aws.sh
@@ -7,3 +7,24 @@ fi
 if [[ -f /usr/local/bin/aws_completer ]]; then
 	complete -C '/usr/local/bin/aws_completer' aws
 fi
+
+#StackStatusFunction
+function stackstatus {
+	while [[ 1 ]]; do
+		response=$(aws cloudformation describe-stacks --stack-name $1)
+		response=$(echo "$response" | tr '\n' ' ' | tr -s " " | sed -e 's/^ *//' -e 's/ *$//')
+		stackStatus=$(echo $response | sed -e 's/^.*"StackStatus"[ ]*:[ ]*"//' -e 's/".*//')
+
+		echo "Stack status: $stackStatus"
+
+		if [ "$stackStatus" = "CREATE_COMPLETE" ] || [ "$stackStatus" = "UPDATE_COMPLETE" ]; then
+			break
+		elif [[ $stackStatus != *_IN_PROGRESS ]]; then
+			echo "Error deploying stack '$1', status: $stackStatus"
+			break
+		fi
+
+		sleep 30
+	done
+}
+
