87733ddea2050f07679d5334d404e10fcaca7c8f
Lantrix <lantrix@pobox.com>
2019-05-16 13:31:07 +1000

    Shuffling around node stuff

--- home/.bash_profile.d/000-keg-only.sh
+++ home/.bash_profile.d/000-keg-only.sh
@@ -1,4 +1,1 @@
 #Keg Only brews
-if [[ -d /usr/local/opt/node@8/bin ]]; then
-	export PATH="/usr/local/opt/node@8/bin:$PATH"
-fi
--- home/.bash_profile.d/004-node.sh
+++ home/.bash_profile.d/004-node.sh
@@ -6,5 +6,7 @@ if [[ -d ${HOME}/.npm-packages ]]; then
   # Unset manpath so we can inherit from /etc/manpath via the `manpath` command
   unset MANPATH
   export MANPATH="$NPM_PACKAGES/share/man:$(manpath)"
-fi
 
+  # Tell Node about these packages
+  NODE_PATH="$NPM_PACKAGES/lib/node_modules:$NODE_PATH"
+fi
