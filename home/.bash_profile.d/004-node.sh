7acd32cb182aec3bea8f92eb8e14fad3b43b822c
Lantrix <lantrix@pobox.com>
2019-05-16 15:42:00 +1000

    Nvm and prefix not happy together

--- home/.bash_profile.d/004-node.sh
+++ home/.bash_profile.d/004-node.sh
@@ -1,12 +1,12 @@
-if [[ -d ${HOME}/.npm-packages ]]; then
-  NPM_PACKAGES="${HOME}/.npm-packages"
+# if [[ -d ${HOME}/.npm-packages ]]; then
+#   NPM_PACKAGES="${HOME}/.npm-packages"
 
-  PATH="$NPM_PACKAGES/bin:$PATH"
+#   PATH="$NPM_PACKAGES/bin:$PATH"
 
-  # Unset manpath so we can inherit from /etc/manpath via the `manpath` command
-  unset MANPATH
-  export MANPATH="$NPM_PACKAGES/share/man:$(manpath)"
+#   # Unset manpath so we can inherit from /etc/manpath via the `manpath` command
+#   unset MANPATH
+#   export MANPATH="$NPM_PACKAGES/share/man:$(manpath)"
 
-  # Tell Node about these packages
-  NODE_PATH="$NPM_PACKAGES/lib/node_modules:$NODE_PATH"
-fi
+#   # Tell Node about these packages
+#   NODE_PATH="$NPM_PACKAGES/lib/node_modules:$NODE_PATH"
+# fi
