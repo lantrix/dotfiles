cddf85816917a3681caf2dea68a207946fa82574
Lantrix <lantrix@pobox.com>
2019-05-17 14:44:12 +1000

    Switch to pyenv

--- home/.bash_profile.d/011-mac-python3.sh
+++ home/.bash_profile.d/011-mac-python3.sh
@@ -1,7 +1,3 @@
 if [[ `uname` -eq 'Darwin' ]]; then
-	export PATH="/usr/local/opt/python/libexec/bin:$PATH"
-	export PATH=$PATH:/usr/local/sbin
-	if [[ -d $HOME/Library/Python/3.6/bin  ]]; then
-		export PATH="$HOME/Library/Python/3.6/bin:$PATH"
-	fi
+	eval "$(pyenv init -)"
 fi
