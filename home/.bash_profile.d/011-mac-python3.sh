137434ebedb4a1d58b22fecef8f1e01b5aee5d6b
Lantrix <lantrix@pobox.com>
2019-05-18 14:15:28 +1000

    Pyenv and Virtual env

--- home/.bash_profile.d/011-mac-python3.sh
+++ home/.bash_profile.d/011-mac-python3.sh
@@ -1,3 +1,7 @@
 if [[ `uname` -eq 'Darwin' ]]; then
 	eval "$(pyenv init -)"
+	eval "$(pyenv virtualenv-init -)"
+	#pyenv compile needs zlib - zlib is keg-only
+	export LDFLAGS="-L/usr/local/opt/zlib/lib"
+	export CPPFLAGS="-I/usr/local/opt/zlib/include"
 fi
