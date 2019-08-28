49fcfa03e027cadb9c8e2da063ddeab7741ca04d
Lantrix <lantrix@pobox.com>
2019-08-28 19:00:16 +1000

    Using pyenv now

--- home/.bash_profile.d/006-prompt.sh
+++ home/.bash_profile.d/006-prompt.sh
@@ -1,26 +1,28 @@
 # Powerline
 ## Mac install
-if [[ -f $HOME/Library/Python/3.7/lib/python/site-packages/powerline/bindings/bash/powerline.sh ]];
+userSite=$(python -m site --user-site)
+userBase=$(python -m site --user-base)
+if [[ -f ${userSite}/powerline/bindings/bash/powerline.sh ]];
 then
-  export PATH="$PATH:$HOME/Library/Python/3.7/bin"
+  export PATH="${userBase}/bin:$PATH"
   export POWERLINE_COMMAND=powerline
   export POWERLINE_CONFIG_COMMAND=powerline-config
   powerline-daemon -q
   POWERLINE_BASH_CONTINUATION=1
   POWERLINE_BASH_SELECT=1
-  . $HOME/Library/Python/3.7/lib/python/site-packages/powerline/bindings/bash/powerline.sh
+  . ${userSite}/powerline/bindings/bash/powerline.sh
 fi
 
 ## Linux install
 if [[ -f $HOME/.local/bin/powerline ]];
 then
-  if [ -f ~/.local/lib/python3.7/site-packages/powerline/bindings/bash/powerline.sh ]; then
-    export PATH="$HOME/.local/bin:$PATH"
+  if [ -f ${userSite}/powerline/bindings/bash/powerline.sh ]; then
+    export PATH="${userBase}/bin:$PATH"
     export POWERLINE_COMMAND=powerline
     export POWERLINE_CONFIG_COMMAND=powerline-config
     powerline-daemon -q
     POWERLINE_BASH_CONTINUATION=1
     POWERLINE_BASH_SELECT=1
-    source ~/.local/lib/python3.7/site-packages/powerline/bindings/bash/powerline.sh
+    source ${userSite}/powerline/bindings/bash/powerline.sh
   fi
 fi
