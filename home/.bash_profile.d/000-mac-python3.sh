290f433156b42ac72400c46814693c3c113c4509
Lantrix <lantrix@pobox.com>
2020-03-31 16:25:50 +1100

    pyenv and pipenv

--- home/.bash_profile.d/000-mac-python3.sh
+++ home/.bash_profile.d/000-mac-python3.sh
@@ -1,7 +1,14 @@
 if [[ `uname` -eq 'Darwin' ]]; then
-	eval "$(pyenv init -)"
-	eval "$(pyenv virtualenv-init -)"
-	#pyenv compile needs zlib - zlib is keg-only
-	export LDFLAGS="-L/usr/local/opt/zlib/lib"
-	export CPPFLAGS="-I/usr/local/opt/zlib/include"
+	if [[ -n "$(which pyenv)" ]]; then
+		eval "$(pyenv init -)"
+		# eval "$(pyenv virtualenv-init -)"
+		#pyenv compile needs zlib - zlib is keg-only
+		export LDFLAGS="-L/usr/local/opt/zlib/lib"
+		export CPPFLAGS="-I/usr/local/opt/zlib/include"
+	fi
+	if [[ -n "$(which pipenv)" ]]; then
+		export PIPENV_PYTHON="$HOME/.pyenv/shims/python"
+		export PIPENV_VENV_IN_PROJECT=1
+		eval "$(pipenv --completion)"
+	fi
 fi
