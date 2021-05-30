914630fdce606d138a2a4bd337f6b49e8cd4b91d
Lantrix <lantrix@pobox.com>
2021-05-30 22:12:15 +1000

    Bash completion changes

--- home/.bash_profile.d/001-bash-completion.sh
+++ home/.bash_profile.d/001-bash-completion.sh
@@ -1,12 +1,19 @@
 #Mac Completion
 if [[ ! ${#BREW_PREFIX} -eq 0 ]]; then
 	if [[ -d $BREW_PREFIX/etc/bash_completion.d ]]; then
-  		source $BREW_PREFIX/etc/bash_completion.d/*
+		#bash-completionv2
+		export BASH_COMPLETION_COMPAT_DIR="$BREW_PREFIX/etc/bash_completion.d"
+		[[ -r "$BREW_PREFIX/etc/profile.d/bash_completion.sh" ]] && . "$BREW_PREFIX/etc/profile.d/bash_completion.sh"
+		source $BREW_PREFIX/etc/bash_completion.d/*
 	fi
 fi
 
 #Linux Completion
 if [[ -f /etc/bash_completion ]]; then
+	if [[ -f `which kubectl` ]]; then
+		# populate kubectl autocompletion
+		kubectl completion bash > /etc/bash_completion.d/kubectl
+	fi
 	bash /etc/bash_completion
 fi
 
