60f050e6a825928ed850ba8bc138630c74cb6e14
Lantrix <lantrix@pobox.com>
2021-05-30 22:13:57 +1000

    Always eval agent

--- home/.bash_profile.d/ssh.sh
+++ home/.bash_profile.d/ssh.sh
@@ -1,5 +1,6 @@
 # SSH Agent
-if [[ $(grep ENCRYPTED $HOME/.ssh/id_rsa) == *ENCRYPTED* ]]; then
+# if [[ $(grep ENCRYPTED $HOME/.ssh/id_rsa) == *ENCRYPTED* ]]; then
+if [[ -f $HOME/.ssh/id_rsa ]]; then
 	eval $(ssh-agent)
 	ssh-add $HOME/.ssh/id_rsa
 fi
