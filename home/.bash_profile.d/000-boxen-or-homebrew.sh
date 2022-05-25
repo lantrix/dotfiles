0bea16f38c558e3134088bc3823fc01e52004661
Ted B <lantrix@pobox.com>
2022-05-25 22:32:27 +1000

    Apple Silicon

--- home/.bash_profile.d/000-boxen-or-homebrew.sh
+++ home/.bash_profile.d/000-boxen-or-homebrew.sh
@@ -2,6 +2,9 @@ if [[ -f /opt/boxen/env.sh  ]]; then
 	source /opt/boxen/env.sh
 fi
 
+#Apple Silicon
+[[ -d "/opt/homebrew" ]] && eval "$(/opt/homebrew/bin/brew shellenv)"
+
 if [[ -n "$(which brew 2>/dev/null)" ]]; then
 	export BREW_CELLAR=$(brew --cellar)
 	export BREW_PREFIX=$(brew --prefix)
