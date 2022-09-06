f26fe8be0cb6a7bc06467858a46e0475d5e22340
Lantrix <lantrix@pobox.com>
2022-09-06 23:36:12 +1000

    Mac x86 and arm64

--- home/.bash_profile.d/archflags.sh
+++ home/.bash_profile.d/archflags.sh
@@ -1,2 +1,5 @@
 # universal binaries for homebrew
-export ARCHFLAGS="-arch i386 -arch x86_64"
+case $(uname -m) in
+    x86_64) export ARCHFLAGS="-arch i386 -arch x86_64" ;;
+    arm64)  export ARCHFLAGS="-arch arm64" ;;
+esac
