4d0632098813fae06b6619cd76f669e691431831
Lantrix <lantrix@pobox.com>
2022-10-24 21:44:01 +1100

    Use same location mac or linux

--- home/.bash_profile.d/ssh.sh
+++ home/.bash_profile.d/ssh.sh
@@ -1,13 +1,19 @@
 # SSH Agent
 case $(uname) in
   Linux)
-    export SSH_AUTH_SOCK=~/.1password/agent.sock
+    export SSH_AUTH_SOCK=$HOME/.1password/agent.sock
     ;;
   Darwin)
-    export SSH_AUTH_SOCK=~/Library/Group\ Containers/2BUA8C4S2C.com.1password/t/agent.sock
+    if [[ ! -d "$HOME/.1password"  ]]; then
+	    mkdir -p $HOME/.1password
+    fi
+    if [[ ! -e "$HOME/.1password/agent.sock"  ]]; then
+        ln -s $HOME/Library/Group\ Containers/2BUA8C4S2C.com.1password/t/agent.sock $HOME/.1password/agent.sock
+    fi
+    export SSH_AUTH_SOCK=$HOME/.1password/agent.sock
     ;;
   *)
-		eval $(ssh-agent)
-		ssh-add $HOME/.ssh/id_rsa
+    eval $(ssh-agent)
+    ssh-add $HOME/.ssh/id_rsa
     ;;
 esac
